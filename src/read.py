c0a09e730addc4287137f2c3c2f934bfc758bc49
Joe Oppenheim <joe@joesta.com>
2025-09-21 05:09:18 -0400

    include binary representation

--- src/read.py
+++ src/read.py
@@ -2,7 +2,7 @@ data = open("data/line.txt", "rb")
 
 byte = data.read(1)
 while byte:
-    print(f'{byte}  {byte[0]}')
+    print(f'{byte}  {byte[0]}  {byte[0]:b}')
     byte = data.read(1)
 
 data.close()
